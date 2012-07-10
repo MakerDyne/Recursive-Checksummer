dfa0c0b2c18f9a9ee593b58c78ddc6e48f2046d0
Richard Leszczynski <richard@makerdyne.com>
2012-07-10 22:52:37 +0100

    refined processing of command line arguments

--- Main.cs
+++ Main.cs
@@ -19,6 +19,7 @@ namespace RecursiveChecksummer
 		static string rootsource = null;
 		static string rootdest = null;
 		static string fileWithChecksums;
+		static bool useTemporaryFileWithChecksums = false;
 		static ushort numCores = 0;
 		static StreamWriter fwcWriter;
 		static StreamReader fwcReader;
@@ -45,36 +46,42 @@ Console.WriteLine("Number of command line arguments is " + args.GetLength(0));
 				{
 				case "-s":
 					// signifies that the next CLA will be the root source directory
-					if(fileWithChecksums != null)
-					{
+					if(rootsource != null) {
+						Console.WriteLine("ERROR: multiple source (-s) directories have been specified. Please only specify a single source directory");
+						printUsageInformation();
+						return 1;
+					}
+					if(fileWithChecksums != null) {
 						Console.WriteLine("ERROR: Please specify source (-s) and/or destination (-d) directories before specifying a file to hold checksums");
 						printUsageInformation();
 						return 1;
 					}
-					else
-					{
-						if(!Directory.Exists(rootsource = args[++i]))
-						{
-							Console.WriteLine("ERROR: Source directory does not exist!");
-							return 1;
-						}
+					rootsource = args[++i];
+					if(!rootsource.StartsWith("/"))	// Deal with relative directory paths in the command line args
+						rootsource = Environment.CurrentDirectory + "/" + rootsource;
+					if(!Directory.Exists(rootsource)) {
+						Console.WriteLine("ERROR: Source (-s) directory does not exist!");
+						return 1;
 					}
 					break;
 				case "-d":
 					// signifies that the next CLA will be the root destination directory
-					if(fileWithChecksums != null)
-					{
+					if(rootdest != null) {
+						Console.WriteLine("ERROR: multiple destination (-d) directories have been specified. Please only specify a single destination directory");
+						printUsageInformation();
+						return 1;
+					}
+					if(fileWithChecksums != null) {
 						Console.WriteLine("ERROR: Please specify source (-s) and/or destination (-d) directories before specifying a file to hold checksums");
 						printUsageInformation();
 						return 1;
 					}
-					else
-					{
-						if(!Directory.Exists(rootdest = args[++i]))
-						{
-							Console.WriteLine("ERROR: Destination directory does not exist!");
-							return 1;
-						}
+					rootdest = args[++i];
+					if(!rootdest.StartsWith("/"))	// Deal with relative directory paths in the command line args
+						rootdest = Environment.CurrentDirectory + "/" + rootdest;
+					if(!Directory.Exists(rootdest)) {
+						Console.WriteLine("ERROR: Destination (-d) directory does not exist!");
+						return 1;
 					}
 					break;
 				case "-f":
@@ -87,62 +94,35 @@ Console.WriteLine("Number of command line arguments is " + args.GetLength(0));
 					 * 3. if -d is already specified, -f signifies an existing file which contains the checksums that all files in destsource are to be checked against
 					 * TODO: decide whether to keep the second option
 					 */
-					fileWithChecksums = args[++i];
-					if((rootdest == null) && (rootdest == null))
-					{
-						// Error condition, need to have already specified either a source OR destination directory
-						Console.WriteLine("ERROR: Please specify EITHER a source (-s) AND/OR destination (-d) directory before specifying a file (-f) of checksums");
+					if(fileWithChecksums != null) {
+						Console.WriteLine("ERROR: multiple files to hold checksums (-f) have been specified. Please only specify a single file");
 						printUsageInformation();
 						return 1;
 					}
-					else
-					{
-						if(rootsource != null)	// Implies Mode 1 or 2. In the case of Mode 2, the streamreader required for the 2nd half of the operation can only be created later
-						{
-							try
-							{
-								fwcWriter = new StreamWriter(fileWithChecksums, false, Encoding.Default);
-							}
-							catch(Exception ex)
-							{
-								Console.WriteLine("ERROR: Cannot access file {0] to hold checksums in {1}",
-								                  Path.GetFileName(fileWithChecksums), Path.GetDirectoryName(fileWithChecksums));
-								Console.WriteLine(ex.Message);
-								return 1;
-							}
-						}
-						else// Implies Mode 3.
-						{
-							try
-							{
-								fwcReader = new StreamReader(fileWithChecksums, Encoding.Default);
-							}
-							catch(Exception ex)
-							{
-								Console.WriteLine("ERROR: Cannot access file {0] to hold checksums in {1}",
-								                  Path.GetFileName(fileWithChecksums), Path.GetDirectoryName(fileWithChecksums));
-								Console.WriteLine(ex.Message);
-								return 1;
-							}
-						}
+					fileWithChecksums = args[++i];
+					if(!fileWithChecksums.StartsWith("/"))	// Deal with relative directory paths in the command line args
+						fileWithChecksums = Environment.CurrentDirectory + "/" + fileWithChecksums;
+					if(!File.Exists(fileWithChecksums)) {
+						Console.WriteLine("ERROR: File to hold checksums (-f) does not exist!");
+						return 1;
 					}
 					break;
 				case "-c":
 					// determines the number of concurrent instances of md5sum to run. Cannot be greater than the number of processor cores
-					Console.WriteLine("Number of processor cores detected is " + Environment.ProcessorCount);					
-					try
-					{
+					ushort numCoresAvailable = (ushort)Environment.ProcessorCount;
+					if(numCoresAvailable<1)
+						numCoresAvailable = 1;
+					try {
 						numCores = Convert.ToUInt16(args[++i]);
 					}
-					catch(Exception ex)
-					{
+					catch(Exception ex) {
 						Console.WriteLine("ERROR: Problem with the value specified for the number of cores (-c) argument: {0}", args[i]);
+						Console.WriteLine(ex.Message);
 						return 1;
 					}
 					if(numCores == 0)
 						numCores=1;
-					else if(numCores > Environment.ProcessorCount)
-					{
+					else if(numCores > Environment.ProcessorCount) {
 						numCores = (ushort)Environment.ProcessorCount;
 					}
 					break;
@@ -156,17 +136,11 @@ Console.WriteLine("Number of command line arguments is " + args.GetLength(0));
 					return 1;
 				}
 			}
-			// Check if there is enough information to continue and set the programMode to 1,2 or 3
-			if((rootsource == null) && (rootdest == null))
-			{
-				Console.WriteLine("Error:");
-			}
-			
+						
 			// PRE-RUN CHECKS
 			// Check that md5sum exists
-			if(!File.Exists(@"/bin/md5sum"))
-			{
-				Console.WriteLine("ERROR: md5sum either does not exist or cannot be found");
+			if(!File.Exists(@"/bin/md5sum")) {
+				Console.WriteLine("ERROR: The program which calculates the checksums (md5sum) either does not exist or cannot be found");
 				return 1;
 			}
 			// Check /tmp or /var/tmp exists and that write permissions are available
@@ -174,26 +148,86 @@ Console.WriteLine("Number of command line arguments is " + args.GetLength(0));
 				tmpdir = @"/tmp/";
 			else if(Directory.Exists(@"/var/tmp/"))
 				tmpdir = @"/var/tmp/";
-			else
-			{
+			else {
 				Console.WriteLine("ERROR: No temporary directory is available for writing working files to");
 				return 1;
 			}
 			string testFile = tmpdir + "RecursiveChecksummerTestFile.txt";
-			try
-			{
+			try {
 				File.Create(testFile);
 			}
-			catch(Exception ex)
-			{
+			catch(Exception ex) {
 				Console.WriteLine("ERROR: Cannot create temporary working files in {0}", tmpdir);
 				Console.WriteLine(ex.Message);
 				return 1;
 			}
 			File.Delete(testFile);
 			
+			// DETERMINE IF PROGRAM IS TO RUN IN MODE 1,2 or 3
+			// Determine Mode 1
+			if((rootsource != null) && (rootdest == null)) {
+				if(fileWithChecksums != null)
+					programMode = 1;
+				else {
+					Console.WriteLine("ERROR: A file to hold checksums (-f) must be specified");
+					printUsageInformation();
+					return 1;
+				}
+			}
+			// Determine Mode 2
+			else if((rootsource != null) && (rootdest != null)) {
+				programMode = 2;
+				if(fileWithChecksums == null) {
+					useTemporaryFileWithChecksums = true;
+					fileWithChecksums = tmpdir + "fileWithChecksums.txt";
+					Console.WriteLine("New location for the file with checksums is {0}", fileWithChecksums);
+				}
+				else
+					useTemporaryFileWithChecksums = false;
+			}
+			// Determine Mode 3
+			else if((rootsource == null) && (rootdest != null)) {
+				if(fileWithChecksums != null)
+					programMode = 3;
+				else {
+					Console.WriteLine("ERROR: A file to hold checksums (-f) must be specified");
+					printUsageInformation();
+					return 1;
+				}
+			}
+			// Determine insufficient arguments for any mode
+			else if((rootsource == null) && (rootdest == null)) {
+				Console.WriteLine("Error: Neither a source (-s) nor destination (-d) directory has been specified");
+				printUsageInformation();
+				return 1;
+			}
+			
+			// Create streams required for modes
+			try {
+				if((programMode == 1) || (programMode == 2))
+					// NB: the streamreader required for the 2nd half of the operation in Mode 2 can only be created later
+					fwcWriter = new StreamWriter(fileWithChecksums, false, Encoding.Default);
+				else if(programMode == 3)
+					fwcReader = new StreamReader(fileWithChecksums, Encoding.Default);
+				else
+					Console.WriteLine("ERROR: An unidentified program mode has been selected. programMode = {0}", programMode);
+			}
+			catch(Exception ex) {
+				Console.WriteLine("ERROR: Cannot access file {0] to hold checksums in {1}",
+				                  Path.GetFileName(fileWithChecksums), Path.GetDirectoryName(fileWithChecksums));
+				Console.WriteLine(ex.Message);
+				return 1;
+			}
+			
+			// TODO: solve issue of relative directory paths...
+			Console.WriteLine("Current working directory is {0}", Environment.CurrentDirectory);
+			Console.WriteLine("Source directory is {0}", rootsource);
+			Console.WriteLine("Destination directory is {0}", rootdest);
+			Console.WriteLine("File with checksums is {0}", fileWithChecksums);
+			
 			// Run a recursive function on the supplied source directory
 			// processDirectory(rootsource);
+			
 			
 			return 0;
 		}
@@ -205,13 +239,11 @@ Console.WriteLine("Number of command line arguments is " + args.GetLength(0));
 			string[] files = Directory.GetFiles(directory);
 			string[] dirs = Directory.GetDirectories(directory);
 			
-			foreach(string file in files)
-			{
+			foreach(string file in files) {
 				createChecksum(file);
 				//total_files++;				
 			}
-			foreach(string dir in dirs)
-			{
+			foreach(string dir in dirs) {
 				processDirectory(dir);
 				//total_dirs++;
 			}
